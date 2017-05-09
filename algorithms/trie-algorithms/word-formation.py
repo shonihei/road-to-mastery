90fe7efd668f6788faf76797cd30171566242b0c
shonihei <shonihei@gmail.com>
2017-05-09 10:00:11 -0400

    added trie stuff

---
+++ algorithms/trie-algorithms/word-formation.py
@@ -0,0 +1,38 @@
+# task: given a Trie with number of words in it, determine if a string S can be formed by concatenating two substrings in the Trie
+
+class TrieNode:
+    def __init__(self):
+        self.children = dict()
+        self.end = False
+
+def add(node, string):
+    for char in string:
+        if char not in node.children:
+            node.children[char] = TrieNode()
+        node = node.children[char]
+    node.end = True
+    
+def add_words(node, lst_of_words):
+    for word in lst_of_words:
+        add(node, word)
+
+def is_possible(node, string):
+    i = 0
+    node2 = node
+    size = len(string)
+    while i < size and string[i] in node.children:
+        node = node.children[string[i]]
+        i += 1
+    print(string[0:i])
+    if not node.end:
+        return False
+    while i < size and string[i] in node2.children:
+        node2 = node2.children[string[i]]
+        i += 1
+    if not node.end:
+        return False
+    return True
+    
+words = ["geeks", "forgeeks", "quiz", "geek"]
+A = TrieNode()
+add_words(A, words)
--- data-structures/Trie.py
+++ data-structures/Trie.py
@@ -1,39 +1,64 @@
+# task: implement a trie using a hashmap to store pointers to children
+
 class TrieNode:
-    ALPHABET = 26
-    
-    def __init__(self, is_end_of_word=False):
-        self.children = [None for character in range(self.ALPHABET)]
-        self.is_end = is_end_of_word
-        
-class Trie:
-
     def __init__(self):
-        self.root = TrieNode()
+        self.children = dict()
+        self.word_end = False
 
-    def add(self, string):
-        cur_node = self.root
-        for c in string:
-            n = cur_node.children[ord(c) - 97]
-            if not n:
-                cur_node.children[ord(c) - 97] = TrieNode()
-            cur_node = cur_node.children[ord(c) - 97]
-        cur_node.is_end = True
+def add_string(cur_node, string):
+    for c in string:
+        if c not in cur_node.children:
+            cur_node.children[c] = TrieNode()
+        cur_node = cur_node.children[c]
+    cur_node.word_end = True
 
-    def contains(self, string):
-        cur_node = self.root
-        for c in string:
-            n = cur_node.children[ord(c) - 97]
-            if not n:
-                return False
-            cur_node = cur_node.children[ord(c) - 97]
-        if cur_node.is_end:
-            return True
-        else:
+def contains(cur_node, string):
+    for c in string:
+        if c not in cur_node.children:
             return False
+        cur_node = cur_node.children[c]
+    if cur_node.word_end:
+        return True
+    return False
 
-a = Trie()
-a.add("shonihei")
-a.add("shoniher")
-a.add("shonihe")
-a.add("shoniheiner")
-print(a.contains("shonihei"))
+def search(node, pattern):
+    lst = []
+    def __search(node, string, result=""):
+        nonlocal lst
+        if not node:
+            return
+        if not string and not node.word_end:
+            return
+        if not string and node.word_end:
+            lst.append(result)
+            return
+        elif string[0] == "*":
+            if node.word_end:
+                lst.append(result)
+            for k, v in node.children.items():
+                __search(v, string, result + k)
+        elif string[0] == "?":
+            for k, v in node.children.items():
+                __search(v, string[1:], result + k)
+        else:
+            if string[0] in node.children:
+                __search(node.children[string[0]], string[1:], result + string[0])
+    __search(node, pattern)
+    return lst
+
+A = TrieNode()
+add_string(A, "shonihei")
+add_string(A, "sho")
+add_string(A, "shone")
+add_string(A, "showtime")
+add_string(A, "shower")
+add_string(A, "shoulder")
+add_string(A, "shallow")
+add_string(A, "shoes")
+add_string(A, "shot")
+add_string(A, "shulker")
+add_string(A, "bass")
+add_string(A, "debaser")
+add_string(A, "degrade")
+add_string(A, "demonstration")
+add_string(A, "!!@$%!@%^!@#!@$%")
