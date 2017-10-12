51c3a526cbf6cf1e12216596658f175a5893a06e
shonihei <shonihei@gmail.com>
2017-10-12 10:05:15 -0400

    strStr implemented using KMP algorithm (probabaly an overkill tho :/ )

--- leetcode/implement-strStr.py
+++ leetcode/implement-strStr.py
@@ -2,25 +2,35 @@
 Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 """
 
-def strStr(haystack, needle):
-    if haystack == needle:
-        return 0
-    if len(needle) == 0:
-        return 0
-    if len(needle) > len(haystack):
-        return -1
-    for i in range(len(haystack)):
-        if haystack[i] == needle[0]:
-            j = i + 1
-            for k in range(1, len(needle)):
-                if j >= len(haystack):
-                    break
-                if needle[k] != haystack[j]:
-                    break
+def prefix(s):
+    p = [0 for i in range(len(s))]
+    i = 0
+    j = 1
+    n = len(s)
+    while j < n:
+        if s[i] == s[j]:
+            i += 1
+            p[j] = i
+            j += 1
+        else:
+            if i == 0:
                 j += 1
             else:
-                return i
+                i = p[i - 1]
+    return p
+
+def strStr(haystack, needle):
+    p = prefix(needle)
+    j = 0
+    for i in range(len(haystack)):
+        while j > 0 and haystack[i] != needle[j]:
+            j = p[j - 1]
+        if haystack[i] == needle[j]:
+            j += 1
+        if j == len(needle):
+            return i - (j - 1)
     return -1
+            
 
 if __name__ == "__main__":
     import unittest
